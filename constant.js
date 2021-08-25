2139a9e34dc9fdada44465405fea1cb82cf9afdc
Luthfi Abdul Azis <luthfieabdulazis#gmail.com>
2021-08-25 18:16:52 +0700

    allowed files

---
+++ constant.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://api.harmonica.app'
+const MY = BASE_URL + '/dating/v1/my/'
+const CARD = MY + 'card/'
+const CARDS = MY + 'cards/'
+
+module.exports = {
+    BASE_URL, MY, CARD, CARDS
+}
