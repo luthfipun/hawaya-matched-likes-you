1af32af6d67d90ea5d33e3324b4407e0c36853d3
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2022-06-01 22:30:44 +0700

    fix find and match

--- app.js
+++ app.js
@@ -1,101 +1,121 @@
-const prompt = require('prompt');
-const { logProgress, logSuccess, logError, logWarning, log, logInfo } = require('./logging');
-const { PATH_DISCOVERY, PATH_NEW_PEOPLE, PATH_RECENTLY_ONLINE, PATH_MATCHING_INTEREST, PATH_MATCHING_SEARCH_CRITERIA } = require('./path');
-const { getLikes, getFeed, postLikes } = require('./services');
+const prompt = require("prompt");
+const {
+  logProgress,
+  logSuccess,
+  logError,
+  logWarning,
+  log,
+  logInfo,
+} = require("./logging");
+const {
+  PATH_DISCOVERY,
+  PATH_NEW_PEOPLE,
+  PATH_RECENTLY_ONLINE,
+  PATH_MATCHING_INTEREST,
+  PATH_MATCHING_SEARCH_CRITERIA,
+} = require("./path");
+const { getLikes, getFeed, postLikes } = require("./services");
+const sleep = require("./utils");
 
-var globalToken = ''
-var likesData = []
-var isFinding = false
-var excluding = []
+var globalToken = "";
+var likesData = [];
+var isFinding = false;
+var excluding = [];
 
 prompt.start();
 
-prompt.get(['token'], async function(err, res){
-    if(err) return logError(err);
-    if(!res.token) return logWarning('Token is empty!')
-    globalToken = res.token
-    await findLikes()
-
+prompt.get(["token"], async function (err, res) {
+  if (err) return logError(err);
+  if (!res.token) return logWarning("Token is empty!");
+  globalToken = res.token;
+  await findLikes();
 });
 
-async function findLikes(){
-    logProgress('Finding likes data...')
-    await getLikes(globalToken)
-        .then(async (res) => {
-            let resData = res.data
-            if (resData.total > 0){
-                logInfo(`Found ${resData.total} likes you`)
-                let cards = resData.cards
-                cards.map((e) => {
-                    e.matchProfile.pictures.map((s) => {
-                        likesData = [...likesData, s.id]
-                    })
-                })
-                await findMathes()
-            }else {
-                logWarning('No likes data found')
-                return 1
-            }
-        })
-        .catch((e) => {
-            logError(e.message)
-            return 1
-        })
+async function findLikes() {
+  logProgress("Finding likes data...");
+  await getLikes(globalToken)
+    .then(async (res) => {
+      let resData = res.data;
+      if (resData.total > 0) {
+        logInfo(`Found ${resData.total} likes you`);
+        let cards = resData.cards;
+        cards.map((e) => {
+          e.matchProfile.pictures.map((s) => {
+            likesData = [...likesData, s.id];
+          });
+        });
+        await findMathes();
+      } else {
+        logWarning("No likes data found");
+        return 1;
+      }
+    })
+    .catch((e) => {
+      logError(e.message);
+      return 1;
+    });
 }
 
-async function findMathes(){
-    log(likesData)
-    isFinding = true
-    logProgress('Staring find matches...')
-    while(isFinding){
-        await findFeeds(PATH_DISCOVERY)
-        await findFeeds(PATH_NEW_PEOPLE)
-        await findFeeds(PATH_RECENTLY_ONLINE)
-        await findFeeds(PATH_MATCHING_INTEREST)
-    }
+async function findMathes() {
+  log(likesData);
+  isFinding = true;
+  logProgress("Staring find matches...");
+  while (isFinding) {
+    await findFeeds(PATH_DISCOVERY);
+    await findFeeds(PATH_NEW_PEOPLE);
+    await findFeeds(PATH_RECENTLY_ONLINE);
+    await findFeeds(PATH_MATCHING_INTEREST);
+  }
 }
 
-async function findFeeds(path){
-    logProgress(`Try find on ${path}`)
-    await getFeed(path, globalToken, excluding.join(','))
-        .then(async (res) => {
-            let data = res.data.cards
-            if(data.length > 0){
-                logInfo(`Getting ${data.length} on ${path}`)
-                let usrIds = data.map(e => e.matchProfile.userId)
-                excluding = [...excluding, usrIds]
-                await data.forEach(async element => {  
-                    if(element.matchProfile.pictures.length > 0){
-                        await element.matchProfile.pictures.forEach(async picItem => {
-                            if(likesData.includes(picItem.id)){
-                                logInfo(`Horrayy! finding matched likes with ${element.matchProfile.firstName} | ID : ${element.matchProfile.userId}`)
-                                await doMatched(element.matchProfile.userId, element.matchProfile.firstName)
-                            }
-                        })
-                    }
-                })
-            }else {
-                logInfo(`${path} not found data`)
-                return
-            }
-        })
-        .catch((e) => {
-            logError(e.message)
-            return 1
-        })
+async function findFeeds(path) {
+  logProgress(`Try find on ${path}`);
+  await getFeed(path, globalToken, excluding.join(","))
+    .then(async (res) => {
+      const data = res.data.cards;
+      if (data !== undefined) {
+        logInfo(`Getting ${data.length} on ${path}`);
+        // let usrIds = data.map((e) => e.matchProfile.userId);
+        // excluding = excluding.concat(usrIds);
+        await data.forEach(async (element) => {
+          if (element.matchProfile.pictures.length > 0) {
+            await element.matchProfile.pictures.forEach(async (picItem) => {
+              if (likesData.includes(picItem.id)) {
+                logInfo(
+                  `Horrayy! finding matched likes with ${element.matchProfile.firstName} | ID : ${element.matchProfile.userId}`
+                );
+                await sleep();
+                await doMatched(
+                  element.matchProfile.userId,
+                  element.matchProfile.firstName
+                );
+              }
+            });
+          }
+        });
+        return;
+      } else {
+        logInfo(`${path} not found data`);
+        return;
+      }
+    })
+    .catch((e) => {
+      logError(e.message);
+      return 1;
+    });
 }
 
-async function doMatched(userId, name){
-    logProgress(`Processing matched to ${name}`)
-    await postLikes(globalToken, userId)
-        .then((res) => {
-            if(res.data.matched){
-                logSuccess(`Congrats! your are matches with ${name} now`)
-            }else {
-                logWarning(`Oops! not matched with ${name} right now`)
-            }
-        })
-        .catch((e) => {
-            logError(e.message)
-        })
+async function doMatched(userId, name) {
+  logProgress(`Processing matched to ${name}`);
+  await postLikes(globalToken, userId)
+    .then((res) => {
+      if (res.data.matched) {
+        logSuccess(`Congrats! your are matches with ${name} now`);
+      } else {
+        logWarning(`Oops! not matched with ${name} right now`);
+      }
+    })
+    .catch((e) => {
+      logError(e.message);
+    });
 }
--- services.js
+++ services.js
@@ -1,49 +1,49 @@
 const { default: axios } = require("axios");
 const { PATH_LIKES, PATH_DO_LIKE } = require("./path");
 
-function options(token){
-    return {
-        headers: {
-            'Authorization': token,
-            'devicetype': 'IOS',
-            'accept': 'application/json, text/plain, */*',
-            'appversion': '4.10.15',
-            'accept-language': 'en',
-            'accept-encoding': 'gzip, deflate, br',
-            'user-agent': 'hawaya/479 CFNetwork/1240.0.4 Darwin/20.5.0',
-            'device-country': 'ID',
-            'devicemodel': 'iPhone SE',
-            'osversion': '14.6'
-        }
-    }
+function options(token) {
+  return {
+    headers: {
+      Authorization: token,
+      devicetype: "IOS",
+      accept: "application/json, text/plain, */*",
+      appversion: "4.21.1",
+      "accept-language": "en",
+      "accept-encoding": "gzip, deflate, br",
+      "user-agent": "hawaya/827 CFNetwork/1329 Darwin/21.3.0",
+      "device-country": "ID",
+      devicemodel: "iPhone SE",
+      osversion: "15.3.1",
+      "Content-Type": "application/json",
+    },
+  };
 }
 
-async function getLikes(token){
-    return await axios.get(PATH_LIKES, options(token))
+async function getLikes(token) {
+  return await axios.get(PATH_LIKES, options(token));
 }
 
-async function postLikes(token, id){
-    let data = {
-        userId: id,
-        likeType: 'LIKE',
-        likedItemId: '',
-        source: 'DISCOVERY'
-    }
+async function postLikes(token, id) {
+  let data = {
+    userId: id,
+    likeType: "LIKE",
+    likedItemId: "",
+    source: "DISCOVERY",
+  };
 
-    let option = {
-        headers: {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-        }
-    }
-
-    return await axios.post(PATH_DO_LIKE, JSON.stringify(data), option)
+  return await axios.post(
+    PATH_DO_LIKE + '?rewind=false"',
+    JSON.stringify(data),
+    options(token)
+  );
 }
 
-async function getFeed(path, token, excludes){
-    return await axios.get(path + excludes, options(token))
+async function getFeed(path, token, excludes) {
+  return await axios.get(path + excludes, options(token));
 }
 
 module.exports = {
-    getLikes, getFeed, postLikes
-}
+  getLikes,
+  getFeed,
+  postLikes,
+};
